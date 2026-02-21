460463142d5b86d456e79c2507690dd3e369d759
MiniCash <tminecraft2143@gmail.com>
2026-02-21 19:27:47 +0900

    Initialize Minicashtestmod with basic setup

---
+++ src/main/java/com/example/minicashtestmod/Minicashtestmod.java
@@ -0,0 +1,57 @@
+package com.example.minicashtestmod;
+
+import com.example.minicashtestmod.registry.MBlocks;
+import com.example.minicashtestmod.registry.MTabs;
+import com.example.minicashtestmod.registry.mItems;
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(Minicashtestmod.MOD_ID)
+public class Minicashtestmod {
+
+    public static final String MOD_ID = "minicashtestmod";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+
+    public Minicashtestmod(FMLJavaModLoadingContext context) {
+        IEventBus modEventBus = context.getModEventBus();
+
+    
+
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+
+    
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
